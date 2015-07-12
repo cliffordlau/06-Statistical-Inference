0ec59a720a7b3980099aef7ab26bb117dea26eaa
clau <cliffordlau000@gmail.com>
2015-07-12 15:48:36 +0800

    set up of the project

---
+++ Statistical Inference Course Project/Analysis.R
@@ -0,0 +1,11 @@
+# 0. Intro ----------------------------------------------------------------
+# Use simulation to explore inference and do some simple inferential data 
+# analysis. 
+# The project consists of two parts:
+#
+# 1. A simulation exercise.
+# 2. Basic inferential data analysis.
+#
+# Each pdf report should be no more than 3 pages with 3 pages of 
+# supporting appendix material
+# 
