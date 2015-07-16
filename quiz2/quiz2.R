f9f158ee5645374f1dc28fe5373c04af844a0579
clau <cliffordlau000@gmail.com>
2015-07-16 17:26:53 +0800

    done q2 10/10

--- quiz2/quiz2.R
+++ quiz2/quiz2.R
@@ -31,3 +31,30 @@ qnorm(0.95,1100, 75)
 # the sample mean of 100 random adult women from this population. What 
 # is the 95th percentile of the distribution of that sample mean?
 
+qnorm(0.95, 1100, 75/sqrt(100))
+
+# Q5: You flip a fair coin 5 times, about what's the probability of 
+# getting 4 or 5 heads?
+1 - pbinom(3, 5, 0.5)
+dbinom(4,5,0.5) + dbinom(5,5,0.5)
+
+# Q6: The respiratory disturbance index (RDI), a measure of sleep 
+# disturbance, for a specific population has a mean of 15 (sleep 
+# events per hour) and a standard deviation of 10. They are not 
+# normally distributed. Give your best estimate of the probability 
+# that a sample mean RDI of 100 people is between 14 and 16 events per hour?
+
+pnorm(16, 15, 10/sqrt(100)) - pnorm(14, 15, 10/sqrt(100))
+
+# Q7: Consider a standard uniform density. The mean for this density is 
+# .5 and the variance is 1 / 12. You sample 1,000 observations from this 
+# distribution and take the sample mean, what value would you expect it 
+# to be near?
+
+0.5
+
+# Q8: The number of people showing up at a bus stop is assumed to be 
+# Poisson with a mean of 5 people per hour. You watch the bus stop for 
+# 3 hours. About what's the probability of viewing 10 or fewer people?
+
+ppois(10, 15)
