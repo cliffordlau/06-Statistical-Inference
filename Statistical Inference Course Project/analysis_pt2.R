d6e33f14d8b62bbdb9a8cfb2dcc51dfccae41d00
clau <cliffordlau000@gmail.com>
2015-07-26 19:42:44 +0900

    part2

---
+++ Statistical Inference Course Project/analysis_pt2.R
@@ -0,0 +1,16 @@
+# 0. Intro ----------------------------------------------------------------
+# Use simulation to explore inference and do some simple inferential data 
+# analysis. 
+# The project consists of two parts:
+#
+# 1. A simulation exercise.
+# 2. Basic inferential data analysis.
+#
+# Each pdf report should be no more than 3 pages with 3 pages of 
+# supporting appendix material
+# 
+
+
+# 1. Librarys -------------------------------------------------------------
+library(ggplot2)
+library(dplyr)
